c9e9e888e863dfa04204e68bfdf87382c838bbe9
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-22 16:40:34 -0300

    Reading by Buffered

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,49 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) {
+
+		String path = "c:\\temp\\inb.txt";
 		
-		
-		
-		
-		
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+
+		catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+
+		finally {
+			if (br != null) {
+				try {
+					br.close();
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (fr != null) {
+				try {
+					fr.close();
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
 	}
 }
