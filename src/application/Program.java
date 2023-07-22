d43b637c181ab4df5530f4381423fd158c95b1e9
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-22 16:03:48 -0300

    First commit

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
